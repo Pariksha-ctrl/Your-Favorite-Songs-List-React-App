128ec0158f6a386e63f1b87a3201fec0dad600ca
Pariksha Roka <parikshanepali@gmail.com>
2020-12-22 02:00:03 +1100

    All the initial set up with expo extension has been done and the initial stage of an app displaying "Hello React Native" has been done.

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// View - map to UI View or an Android View
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ ~/.bash_profile
@@ -0,0 +1,1 @@
+export ANDROID_SDK=C:\Users\parik\android_studio
