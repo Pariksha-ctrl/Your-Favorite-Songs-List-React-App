4956497e7c3910bcc2cba4090ac27d6e7c243ecb
Pariksha Roka <parikshanepali@gmail.com>
2020-12-22 23:22:40 +1100

    Changed the background color of the app and imported SafeAreaView for the proper item alignments.

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 
 // View - map to UI View or an Android View
 export default function App() {
 
+  let x = 1;
   console.log("App executed");
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
