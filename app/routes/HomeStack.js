72e7ca37f04750bf3b3fd6fe68a915256fad32a4
Pariksha Roka <parikshanepali@gmail.com>
2020-12-25 01:09:05 +1100

    Stacked the needed screens.

---
+++ app/routes/HomeStack.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import RegisterScreen from "../screens/RegisterScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
+
+const screens = {
+  WelcomeScreen: {
+    screen: WelcomeScreen,
+  },
+  RegisterScreen: {
+    screen: RegisterScreen,
+  },
+};
+
+const HomeStack = createStackNavigator({});
+
+export default createAppContainer(HomeStack);
