fcc317a98c57f1e56c435184e4bb7e68cfe7b301
Pariksha Roka <parikshanepali@gmail.com>
2020-12-25 10:45:27 +1100

    Made the name passed from the Welcome Screen to render to the Register Screen.

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+// Register Screen
+export default class RegisterScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam("name"),
+    };
+  };
+
+  render() {
+    const { navigate, state } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Text>Hello {state.params.name}</Text>
+
+        <Button
+          title="Go to Main Screen"
+          onPress={() => navigate("MainScreen")}
+        />
+      </View>
+    );
+  }
+}
