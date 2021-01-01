c428125ae6f5ce7d2c9f42cee2fef5a2d3470173
Pariksha Roka <parikshanepali@gmail.com>
2021-01-01 16:15:32 +1100

    Added new screen called "PracticeScreen" to practice something new.

---
+++ app/screens/PracticeScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.containter}>
+      <Text>Let's try something new!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containter: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
